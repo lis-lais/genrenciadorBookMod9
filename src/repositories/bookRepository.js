b78413102fac83408bb74fff05dc5375233239da
Laís Lima <143414398+lis-lais@users.noreply.github.com>
2025-02-16 11:13:53 -0300

    Update bookRepository.js
    Corrigido o erro em updateBook.

--- src/repositories/bookRepository.js
+++ src/repositories/bookRepository.js
@@ -3,7 +3,7 @@ const Book = require('../models/book');
 const createBook = async (bookData) => await Book.create(bookData);
 const findAllBooks = async () => await Book.find ();
 const findBookById = async (id) => await Book.findById (id);
-const updateBook = async (id, bookData) => await Book.findByIdAndUpdate (id, bookData, { new: true });
+const updateBook = async (id, data) => await Book.findByIdAndUpdate (id, data, { new: true });
 const deleteBook = async (id) => await Book.findByIdAndDelete (id);
 const searchBooks = async (filters) => await Book.find(filters);
 
